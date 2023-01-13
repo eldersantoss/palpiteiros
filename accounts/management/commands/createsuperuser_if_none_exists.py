7a9c1079cd4634ad8c6ebb9162220bea8f5594e4
hostsolucoes <elder_rn@hotmail.com>
2023-01-13 10:24:13 -0300

    Create new manage command createsuperuser_if_none_exists

---
+++ accounts/management/commands/createsuperuser_if_none_exists.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    """Create a superuser if none exist.
+
+    Example:
+        python manage.py createsuperuser_if_none_exists \
+            --username=admin --password=admin --email=admin@email.com"""
+
+    def add_arguments(self, parser):
+        parser.add_argument("--username", required=True)
+        parser.add_argument("--password", required=True)
+        parser.add_argument("--email", default="admin@email.com")
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if User.objects.exists():
+            return
+        username = options["user"]
+        password = options["password"]
+        email = options["email"]
+        User.objects.create_superuser(username=username, password=password, email=email)
+        self.stdout.write(f'Superuser "{username}" was created')
