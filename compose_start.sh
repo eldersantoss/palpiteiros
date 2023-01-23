956dbcdfc0e696f854180dd63c3d90b6c9945e78
hostsolucoes <elder_rn@hotmail.com>
2023-01-23 20:20:21 -0300

    Update railway and compose start scripts

--- compose_start.sh
+++ compose_start.sh
@@ -1,7 +1,19 @@
 pip install -r requirements.txt
 python manage.py migrate
+python manage.py collectstatic --noinput
 python manage.py createsuperuser_if_none_exists \
     --username=$ADMIN_USERNAME \
     --password=$ADMIN_PASSWORD \
     --email=$ADMIN_EMAIL
-python manage.py runserver 0.0.0.0:8000
+if [ "$DEBUG" = "True" ]; then
+    python manage.py runserver 0.0.0.0:8000
+else
+    gunicorn \
+        --bind 0.0.0.0:8000 \
+        --workers 2 \
+        --worker-class gevent \
+        --log-level DEBUG \
+        --access-logfile "-" \
+        --error-logfile "-" \
+        palpiteiros.wsgi
+fi
--- railway_start.sh
+++ railway_start.sh
@@ -4,10 +4,15 @@ python manage.py createsuperuser_if_none_exists \
     --username=$ADMIN_USERNAME \
     --password=$ADMIN_PASSWORD \
     --email=$ADMIN_EMAIL
-gunicorn \
-    --workers 2 \
-    --worker-class gevent \
-    --log-level DEBUG \
-    --access-logfile "-" \
-    --error-logfile "-" \
-    palpiteiros.wsgi
+if [ "$DEBUG" = "True" ]; then
+    python manage.py runserver 0.0.0.0:$PORT
+else
+    gunicorn \
+        --bind 0.0.0.0:$PORT \
+        --workers 2 \
+        --worker-class gevent \
+        --log-level DEBUG \
+        --access-logfile "-" \
+        --error-logfile "-" \
+        palpiteiros.wsgi
+fi
