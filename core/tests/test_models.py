6bf92d454413cc0d6220ef4cce92e2075261260d
hostsolucoes <elder_rn@hotmail.com>
2023-02-04 18:52:41 -0300

    Adicona teste de método aberta_para_palpites

---
+++ core/tests/test_models.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+from django.test import TestCase
+from django.utils import timezone
+
+from model_mommy import mommy
+
+from ..models import Partida
+
+
+class PartidaModelTests(TestCase):
+    def test_open_to_predictions_half_hour_before_match_time(self):
+        """Should return True if the match time is at least half an
+        hour before current time and False otherwise"""
+
+        open_match = mommy.make(
+            Partida,
+            data_hora=timezone.now() + timedelta(minutes=31),
+        )
+        on_limit_closed_match = mommy.make(
+            Partida,
+            data_hora=timezone.now() + timedelta(minutes=30),
+        )
+        self.assertTrue(open_match.aberta_para_palpites())
+        self.assertFalse(on_limit_closed_match.aberta_para_palpites())
