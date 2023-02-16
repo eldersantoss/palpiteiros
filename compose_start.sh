c068f10ff2db377775b5012f09eb09029fc87477
Elder Santos <elder_rn@hotmail.com>
2023-02-16 12:49:49 -0300

    chore: adc Dockerfile, atualiza docker-compose e deleta pasta docker (#26)

--- compose_start.sh
+++ compose_start.sh
@@ -1,13 +1,12 @@
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py createsuperuser_if_none_exists \
-    --username=$ADMIN_USERNAME \
-    --password=$ADMIN_PASSWORD \
-    --email=$ADMIN_EMAIL
 if [ "$DEBUG" = "True" ]; then
     python manage.py runserver 0.0.0.0:8000
 else
+  python manage.py migrate
+  python manage.py collectstatic --noinput
+  python manage.py createsuperuser_if_none_exists \
+      --username=$ADMIN_USERNAME \
+      --password=$ADMIN_PASSWORD \
+      --email=$ADMIN_EMAIL
     gunicorn \
         --bind 0.0.0.0:8000 \
         --workers 2 \
