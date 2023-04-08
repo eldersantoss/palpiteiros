3fad2c61edac77b79aea42eb185c78a6f7b2aed6
hostsolucoes <elder_rn@hotmail.com>
2023-04-08 16:48:44 -0300

    chore: remove default dev settings from db connection

--- palpiteiros/settings.py
+++ palpiteiros/settings.py
@@ -109,11 +109,11 @@ WSGI_APPLICATION = "palpiteiros.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME", default="palpiteiros"),
-        "USER": config("DB_USER", default="postgres"),
-        "PASSWORD": config("DB_PASSWORD", default="123mudar!"),
-        "HOST": config("DB_HOST", default="postgres"),
-        "PORT": config("DB_PORT", default=5432, cast=int),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     },
 }
 
