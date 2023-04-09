b99726a6b9b764407977543edb98290a8367a189
hostsolucoes <elder_rn@hotmail.com>
2023-04-09 12:12:54 -0300

    feat: updates admin panel to fit with new models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Equipe, Rodada, Partida, Palpiteiro, Palpite
+from . import models
 
 
 admin.site.site_header = "Administração Palpiteiros"
@@ -8,13 +8,13 @@ admin.site.site_title = "Administração Palpiteiros"
 admin.site.index_title = "Selecione entidade para modificar"
 
 
-@admin.register(Equipe)
+@admin.register(models.Equipe)
 class EquipeAdmin(admin.ModelAdmin):
     list_display = ("__str__", "abreviacao")
     search_fields = ("nome", "abreviacao")
 
 
-@admin.register(Palpiteiro)
+@admin.register(models.Palpiteiro)
 class PalpiteiroAdmin(admin.ModelAdmin):
     list_display = ("__str__", "usuario", "guessed_on_last_round")
     search_fields = [
@@ -24,11 +24,10 @@ class PalpiteiroAdmin(admin.ModelAdmin):
     ]
 
 
-@admin.register(Partida)
+@admin.register(models.Partida)
 class PartidaAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
-        "rodada",
         "data_hora",
         "open_to_guesses",
         "gols_mandante",
@@ -39,31 +38,38 @@ class PartidaAdmin(admin.ModelAdmin):
         "gols_mandante",
         "gols_visitante",
     ]
+    filter_horizontal = ["rodada"]
 
 
-class PartidaInline(admin.TabularInline):
-    model = Partida
-    extra = 0
-    ordering = ["data_hora"]
-
-
-@admin.register(Rodada)
+@admin.register(models.Rodada)
 class RodadaAdmin(admin.ModelAdmin):
+    fields = ["pool", "active"]
     list_display = (
         "__str__",
+        "pool",
         "created",
         "modified",
         "number_of_matches",
         "active",
     )
-    prepopulated_fields = {"slug": ("label",)}
-    inlines = [PartidaInline]
 
 
-@admin.register(Palpite)
+@admin.register(models.Palpite)
 class PalpitesAdmin(admin.ModelAdmin):
     list_display = ("__str__", "palpiteiro")
     list_filter = [
         "palpiteiro",
         "partida__rodada",
     ]
+
+
+@admin.register(models.GuessPool)
+class GuessPoolAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "number_of_teams",
+        "number_of_guessers",
+        "number_of_rounds",
+    )
+    prepopulated_fields = {"slug": ("name",)}
+    filter_horizontal = ["teams", "guessers"]
