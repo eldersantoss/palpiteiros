590e7f4e5ea97e3125ada5252a5c394149bd1a48
Elder Santos <elder_rn@hotmail.com>
2023-05-07 19:28:00 -0300

    fix: create Palpiteiro for admin user in createsuperuser_if_none_exists management command

--- accounts/management/commands/createsuperuser_if_none_exists.py
+++ accounts/management/commands/createsuperuser_if_none_exists.py
@@ -1,5 +1,7 @@
-from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+from core.models import Palpiteiro
 
 
 class Command(BaseCommand):
@@ -18,8 +20,16 @@ class Command(BaseCommand):
         User = get_user_model()
         if User.objects.exists():
             return
+
         username = options["username"]
         password = options["password"]
         email = options["email"]
-        User.objects.create_superuser(username=username, password=password, email=email)
+
+        user = User.objects.create_superuser(
+            username=username,
+            password=password,
+            email=email,
+        )
+        Palpiteiro.objects.create(usuario=user)
+
         self.stdout.write(f'Superuser "{username}" was created')
