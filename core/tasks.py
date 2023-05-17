287033503095f446964f1bff1530137070c34ed8
Elder Santos <elder_rn@hotmail.com>
2023-05-17 10:36:15 -0300

    feat: register get_competitions task

--- core/tasks.py
+++ core/tasks.py
@@ -5,6 +5,11 @@ from django.core.management import call_command
 logger = get_task_logger(__name__)
 
 
+@shared_task(name="get_competitions")
+def get_competitions(season: int, league_ids: list[int]):
+    call_command("get_competitions", season, league_ids)
+
+
 @shared_task(name="get_new_matches")
 def get_new_matches(days_ahead: int):
     call_command("get_new_matches", days_ahead)
