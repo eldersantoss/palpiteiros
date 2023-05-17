7d4e01ce0d72eda130e7ddd367d552d4c3b479b1
Elder Santos <elder_rn@hotmail.com>
2023-05-17 09:42:03 -0300

    chore: remove old scripts

--- scripts/railway_db_backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e  # Configure shell so that if one command fails, it exits
-
-railway link $RAILWAY_PROJECT_ID
-railway run pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT -n public -F t $DB_NAME > db/backup/latest.tar
