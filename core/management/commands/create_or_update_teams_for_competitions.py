01090243a1127acc6ffd6af2f02ad2f3b6813862
Elder Santos <elder_rn@hotmail.com>
2024-06-27 18:02:28 -0300

    refactor: standardizes the format of management commands arguments

--- core/management/commands/create_or_update_teams_for_competitions.py
+++ core/management/commands/create_or_update_teams_for_competitions.py
@@ -7,21 +7,19 @@ class Command(BaseCommand):
     help = "Get data from Football API and update teams for the competitions."
 
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("--season", type=int, help="Football API league season", default=None)
         parser.add_argument(
-            "--league_ids", type=int, nargs="+", help="Football API league ids separeted by space", default=[]
+            "league_ids",
+            type=int,
+            nargs="+",
+            help="Football API league ids separeted by space",
+            default=[],
         )
 
     def handle(self, *args, **options):
-        season = options["season"]
         league_ids = options["league_ids"]
 
-        data_source_ids = [league_id + season for league_id in league_ids]
-
         competitions = (
-            Competition.objects.filter(data_source_id__in=data_source_ids)
-            if data_source_ids
-            else Competition.objects.all()
+            Competition.objects.filter(data_source_id__in=league_ids) if league_ids else Competition.objects.all()
         )
 
         for competition in competitions:
