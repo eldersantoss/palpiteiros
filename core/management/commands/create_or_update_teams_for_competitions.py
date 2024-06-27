dd707658db024f23009faf1ab6ca7b7b55ab3fc3
Elder Santos <elder_rn@hotmail.com>
2024-06-27 15:33:57 -0300

    refactor: rename management command that update competition's teams

--- core/management/commands/create_or_update_teams_for_competitions.py
+++ core/management/commands/create_or_update_teams_for_competitions.py
@@ -1,7 +1,3 @@
-from time import sleep
-
-import requests
-from django.conf import settings
 from django.core.management.base import BaseCommand, CommandParser
 
 from core.models import Competition
@@ -11,27 +7,22 @@ class Command(BaseCommand):
     help = "Get data from Football API and update teams for the competitions."
 
     def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("--season", type=int, help="Football API league season", default=None)
         parser.add_argument(
-            "--season",
-            type=int,
-            help="Football API league season",
-            default=None
-        )
-        parser.add_argument(
-            "--league_ids",
-            type=int,
-            nargs="+",
-            help="Football API league ids separeted by space",
-            default=[]
+            "--league_ids", type=int, nargs="+", help="Football API league ids separeted by space", default=[]
         )
 
     def handle(self, *args, **options):
         season = options["season"]
         league_ids = options["league_ids"]
 
-        data_source_ids = [l + season for l in league_ids]
+        data_source_ids = [league_id + season for league_id in league_ids]
 
-        competitions = Competition.objects.filter(data_source_id__in=data_source_ids) if data_source_ids else Competition.objects.all()
+        competitions = (
+            Competition.objects.filter(data_source_id__in=data_source_ids)
+            if data_source_ids
+            else Competition.objects.all()
+        )
 
         for competition in competitions:
             competition.get_teams()
