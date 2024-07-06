d8a436017f7751d6f8b51c68d41ecaa3c14c0cc1
Elder Santos <elder_rn@hotmail.com>
2024-07-05 23:36:29 -0300

    test: remove test of deleted method of competition model

--- core/tests/test_competition_models.py
+++ core/tests/test_competition_models.py
@@ -4,29 +4,7 @@ import pytest
 from django.utils import timezone
 from model_bakery import baker
 
-from ..models import Competition
-
 pytestmark = pytest.mark.django_db
-
-
-@patch("requests.get")
-def test_create_or_update(mock_get, mock_success_response, get_competition_by_id_and_season_response):
-    response_data = get_competition_by_id_and_season_response
-    league_id = response_data["response"][0]["league"]["id"]
-    league_name = response_data["response"][0]["league"]["name"]
-    season = 2024
-
-    mock_success_response.json.return_value = response_data
-    mock_get.return_value = mock_success_response
-
-    Competition.create_or_update(season, league_id)
-
-    mock_get.assert_called_once()
-
-    competitions = Competition.objects.all()
-
-    assert competitions.count() == 1
-    assert competitions.first().name == f"{league_name}"
 
 
 @patch("requests.get")
