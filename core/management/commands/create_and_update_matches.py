41fe4c6468ea8d4b364c7901e9c697221d71ae70
Elder Santos <elder_rn@hotmail.com>
2024-07-06 13:34:06 -0300

    fix: add season param in  create_and_update_matches command

--- core/management/commands/create_and_update_matches.py
+++ core/management/commands/create_and_update_matches.py
@@ -1,5 +1,6 @@
 from django.core.cache import cache
 from django.core.management.base import BaseCommand, CommandParser
+from django.utils import timezone
 
 from core.models import Competition
 
@@ -9,17 +10,26 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
+            "--season",
+            type=int,
+            help="Season of the league whose teams will be created or updated",
+            default=timezone.now().year,
+        )
+        parser.add_argument(
             "--days_from",
             type=int,
             help="Number of days ago from which matches will be fetched",
+            default=3,
         )
         parser.add_argument(
             "--days_ahead",
             type=int,
             help="Number of days ahead to which matches will be fetched",
+            default=3,
         )
 
     def handle(self, *args, **options):
+        season = options.get("season")
         days_from = options.get("days_from")
         days_ahead = options.get("days_ahead")
 
@@ -30,7 +40,7 @@ class Command(BaseCommand):
 
         for comp in competitions:
             try:
-                created, updated = comp.create_and_update_matches(days_from, days_ahead)
+                created, updated = comp.create_and_update_matches(season, days_from, days_ahead)
                 self.stdout.write(f"{len(created)} matches created and {len(updated)} updated matches for {comp}...")
 
             except Exception as e:
