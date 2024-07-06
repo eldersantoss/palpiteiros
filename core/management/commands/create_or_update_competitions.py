421863dcb997fae5911eae09f740c37303152e2d
Elder Santos <elder_rn@hotmail.com>
2024-07-05 23:34:32 -0300

    refact: move requests from Competition model to create_or_update_competition command

--- core/management/commands/create_or_update_competitions.py
+++ core/management/commands/create_or_update_competitions.py
@@ -1,6 +1,11 @@
+import logging
+
 from django.core.management.base import BaseCommand, CommandParser
 
 from core.models import Competition
+from core.services.football import FootballApiService
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -8,23 +13,32 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
-            "season",
-            type=int,
-            help="Football API league season",
-        )
-        parser.add_argument(
             "league_ids",
             type=int,
             nargs="+",
             help="Football API league ids separeted by space",
         )
 
+    # TODO: REMOVER SEASON HARDCODED
     def handle(self, *args, **options):
-        season = options["season"]
         league_ids = options["league_ids"]
 
         for league_id in league_ids:
-            Competition.create_or_update(season, league_id)
+            league_data = FootballApiService.get_league_by_id(league_id)
+
+            if not league_data:
+                logger.warning(f"League {league_id} not found")
+                continue
+
+            data_source_id = league_data["league"]["id"]
+            name = league_data["league"]["name"]
+            competition, created = Competition.objects.update_or_create(
+                data_source_id=data_source_id,
+                defaults={"name": name, "season": 2024},
+            )
+
+            action_performed = "created" if created else "updated"
+            logger.info(f"Competition {competition.name} (id {competition.data_source_id}) was {action_performed}.")
 
         self.stdout.write(
             "Registration of competitions was done. Now, you need register teams for competitions with create_or_update_teams_for_competitions command."
